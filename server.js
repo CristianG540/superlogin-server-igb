18118d824b969b4b345474afc95af75c947391c1
CristianG540 <cristianneatproy@gmail.com>
2017-11-20 16:42:53 -0500

    la base de datos de cliente funciona con el certificado ssl

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(function(req, res, next) {
 
 var config = {
   dbServer: {
-    protocol: 'http://',
-    host: '108.163.227.76:5984',
+    protocol: 'https://',
+    host: '108.163.227.76:6984',
     user: 'admin',
     password: 'Webmaster2017#@',
     userDB: 'sl-users',

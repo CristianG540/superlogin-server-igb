504bbb58b1f24bf75c4536c8e7ecfd920276f49d
CristianG540 <cristianneatproy@gmail.com>
2017-11-21 09:23:57 -0500

    quito el ssl de las bases de datos de los clientes

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use(function(req, res, next) {
 
 var config = {
   dbServer: {
-    protocol: 'https://',
-    host: '108.163.227.76:6984',
+    protocol: 'http://',
+    host: '108.163.227.76:5984',
     user: 'admin',
     password: 'Webmaster2017#@',
     userDB: 'sl-users',
